5208b73c2f07f162e8b12850393238817eaea4a9
Arthur Axel 'fREW' Schmidt <frioux@gmail.com>
2015-07-22 16:06:24 -0700

    look how weird this is

--- fails.sh
+++ fails.sh
@@ -1,5 +1,6 @@
 #!/bin/dash
 
 docker run -d -v $SSH_AUTH_SOCK:/ssh-agent -e SSH_AUTH_SOCK=/ssh-agent --name tester tester perl -E'while (1) { sleep 1 }'
+docker exec tester chown 5000 /ssh-agent
 docker exec --user 5000 tester ssh-add -l
 docker rm -f tester
