e8387828dcf59f93300fff8a2aa16485da2a67b2
Arthur Axel 'fREW' Schmidt <frioux@gmail.com>
2015-07-22 16:04:00 -0700

    poor mans init script

--- fails.sh
+++ fails.sh
@@ -1,5 +1,5 @@
 #!/bin/dash
 
-docker run -d -v $SSH_AUTH_SOCK:/ssh-agent -e SSH_AUTH_SOCK=/ssh-agent --name tester tester bash
+docker run -d -v $SSH_AUTH_SOCK:/ssh-agent -e SSH_AUTH_SOCK=/ssh-agent --name tester tester perl -E'while (1) { sleep 1 }'
 docker exec --user 5000 tester ssh-add -l
 docker rm -f tester
